af371eb18085a4ae30e93bf73e3ca6a67b44101a
32169 <32169@L203-03.step.edu>
2020-06-07 12:48:18 +0300

    1

---
+++ Hello1/src/Hello/Program.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hello;
+
+/**
+ *
+ * @author 32169
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hi");
+        class2 s = new class2();
+        System.out.printf("%d",21);
+    }
+    class class3{
+        
+    }
+    
+    
+}
+class class2{
+    int i = 1;
+    }
