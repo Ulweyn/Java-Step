d6bb90c72afd67bcc911caa4ab2c5f5dbd1633f6
32169 <32169@L203-03.step.edu>
2020-06-07 13:26:54 +0300

    1

--- Hello1/src/Hello/Program.java
+++ Hello1/src/Hello/Program.java
@@ -5,6 +5,10 @@
  */
 package Hello;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 32169
@@ -15,10 +19,35 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hi");
-        class2 s = new class2();
-        System.out.printf("%d",21);
+        
+            // TODO code application logic here
+            System.out.println("Hi");
+            class2 s = new class2();
+            System.out.printf("%d",21);
+            byte[] buf = new byte[256];
+            int sym, n;
+            n=0;
+            try {
+            while((sym=System.in.read())!=10){
+                buf[n++]=(byte)sym;
+            }} catch (IOException ex) {
+                System.out.println(ex.getMessage());
+                return;
+            }
+            
+            /*for(int k =0;k<n;++k){
+                System.out.print(buf[k]+" ");
+                System.out.println((char)buf[k]);
+            }*/
+            for(byte bt:buf){
+                bt='s';
+            }
+            for(byte bt:buf){
+                
+                System.out.print(bt+" ");
+                System.out.println((char)bt);
+            }
+        
     }
     class class3{
         
